d57b4dd85aa0254eeabf82a669919ce34db7f5da
Matheus Maxs <matheusviniciuskarnoski@gmail.com>
2025-09-01 23:17:29 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,6 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
-    },
-  },
-  root: "./client",
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
+  plugins: [react()],
 });
